7f05fd4ca4dcdc3d2f44d447ae1c0948b2c9bcc1
Sahil Malhotra <sahil061626@gmail.com>
2022-05-13 10:22:47 +0530

    Added Price For Product

--- Tangy_DataAccess/Data/ApplicationDbContext.cs
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace Tangy_DataAccess.Data
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ProductPrice> ProductPrices { get; set; }
     }
 }
---
+++ Tangy_Models/DTO/ProductPriceDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models.DTO
+{
+    public class ProductPriceDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Size { get; set; }
+        [Required]
+        public int ProductId { get; set; }
+        [Required]
+        public double Price { get; set; }
+    }
+}
