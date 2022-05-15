d5f4eb18122ee02da4a86b817bd950cd7c008ebc
Sahil Malhotra <sahil061626@gmail.com>
2022-05-15 21:10:49 +0530

    Add Product Page for Client App(WASM).

---
+++ TangyWeb_Client/Service/ProductService.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Tangy_Models.DTO;
+using TangyWeb_Client.Service.IService;
+
+namespace TangyWeb_Client.Service
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+        private string BaseServerUrl;
+
+        public ProductService(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+            BaseServerUrl = _configuration.GetSection("BaseServerUrl").Value;
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetAllProducts()
+        {
+            var response = await _httpClient.GetAsync("/api/products");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var products = JsonConvert.DeserializeObject<IEnumerable<ProductDTO>>(content);
+                foreach (var product in products)
+                {
+                    product.ImageUrl = BaseServerUrl + product.ImageUrl;
+                }
+                return products;
+            }
+            return new List<ProductDTO>();
+        }
+
+        public async Task<ProductDTO> GetProductById(int productId)
+        {
+            var response = await _httpClient.GetAsync($"/api/products/{productId}");
+            var content = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode)
+            {
+                var product = JsonConvert.DeserializeObject<ProductDTO>(content);
+                product.ImageUrl = BaseServerUrl + product.ImageUrl;
+                return product;
+            }
+            else
+            {
+                var errorModel = JsonConvert.DeserializeObject<ErrorModelDTO>(content);
+                throw new Exception(errorModel.ErrorMessage);
+            }
+        }
+    }
+}
---
+++ TangyWeb_Client/ViewModels/DetailsVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Tangy_Models.DTO;
+
+namespace TangyWeb_Client.ViewModels
+{
+    public class DetailsVM
+    {
+        public DetailsVM()
+        {
+            Count = 1;
+            ProductPrice = new();
+        }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter the count greater than 0")]
+        public int Count { get; set; }
+        [Required]
+        public int SelectedPriceId { get; set; }
+        public ProductPriceDTO ProductPrice { get; set; }
+    }
+}
