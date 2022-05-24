026c973be064de4a2bce6e24693716fd6375f3e7
Sahil Malhotra <sahil061626@gmail.com>
2022-05-24 15:25:40 +0530

    Added Authentication for Client Application

---
+++ TangyWeb_Client/Pages/Login.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Web;
+using Tangy_Models.DTO;
+using TangyWeb_Client.Service.IService;
+
+namespace TangyWeb_Client.Pages
+{
+    public partial class Login
+    {
+        public SignInRequestDTO signInRequest { get; set; } = new();
+        public bool IsProcessing { get; set; }
+        public bool IsAuthFailed { get; set; }
+        public string Error { get; set; }
+        [Inject]
+        public NavigationManager _navigationManager { get; set; }
+
+        [Inject]
+        public IAuthenticationService _authService { get; set; }
+
+        public string ReturnURL { get; set; }
+
+        public async Task LoginUser()
+        {
+            IsProcessing = true;
+            var result = await _authService.LoginUser(signInRequest);
+            if (result.IsAuthSuccessful)
+            {
+                var absoluteUri = new Uri(_navigationManager.Uri);
+                var queryParam = HttpUtility.ParseQueryString(absoluteUri.Query);
+                ReturnURL = queryParam["returnUrl"];
+                if (string.IsNullOrWhiteSpace(ReturnURL))
+                {
+                    _navigationManager.NavigateTo("/");
+                }
+                else
+                {
+                    _navigationManager.NavigateTo("/" + ReturnURL);
+                }
+            }
+            else
+            {
+                Error = result.ErrorMessage;
+                IsAuthFailed = true;
+            }
+            IsProcessing = false;
+        }
+    }
+}
---
+++ TangyWeb_Client/Pages/RedirectToLogin.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace TangyWeb_Client.Pages
+{
+    public partial class RedirectToLogin
+    {
+        [Inject]
+        public NavigationManager _navigationManager { get; set; }
+
+        [CascadingParameter]
+        public Task<AuthenticationState> _authState { get; set; }
+
+        public bool NotAuthorized { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await _authState;
+            if (authState?.User?.Identity == null || !authState.User.Identity.IsAuthenticated)
+            {
+                var returnUrl = _navigationManager.ToBaseRelativePath(_navigationManager.Uri);
+                if (string.IsNullOrEmpty(returnUrl))
+                {
+                    _navigationManager.NavigateTo("login");
+                }
+                else
+                {
+                    _navigationManager.NavigateTo($"login?returnUrl={returnUrl}");
+                }
+            }
+            else
+            {
+                NotAuthorized = true;
+            }
+        }
+    }
+}
